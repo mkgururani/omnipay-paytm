728e92576be3ce895bb139e9a40b313b85990ae5
mayank gururani <mayank.gururani@round.glass>
2017-08-21 18:32:52 +0530

    First commit

---
+++ omnipay-paytm/src/Gateway.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Omnipay\Paytm;
+
+use Omnipay\Common\AbstractGateway;
+
+/**
+ * Paytm Gateway
+ *
+ * @link http://paywithpaytm.com/developer.html
+ */
+class Gateway extends AbstractGateway
+{
+    public function getName()
+    {
+        return 'Paytm';
+    }
+
+    public function getDefaultParameters()
+    {
+        return array(
+            'MID' => 'wVhtoq05771472615938',
+            'OrderId' => 'ORDS' . rand(10000, 99999999),
+            'CustomerId' => 'CUST_001',
+            'IndustryType' => 'Retail',
+            'ChannelId' => 'WEB',
+            'TransactionAmount' => 10,
+            'Website' => 'MarketplaceBeta',
+            'MerchantKey' => '5HFgd9GPiwzGOkvKD53N_Vq9SKR8Mmqx',
+            'testMode' => true,
+        );
+    }
+
+    public function getMID()
+    {
+        return $this->getParameter('MID');
+    }
+
+    public function setMID($value)
+    {
+        return $this->setParameter('MID', $value);
+    }
+    
+    public function getMobileNum()
+    {
+    	return $this->getParameter('phoneNum');
+    }
+    
+    public function setMobileNum($value)
+    {
+    	return $this->setParameter('phoneNum', $value);
+    }
+    
+    public function getEmail()
+    {
+    	return $this->getParameter('email');
+    }
+    
+    public function setEmail($value)
+    {
+    	return $this->setParameter('email', $value);
+    }
+	
+    public function getCallBackUrl()
+    {
+    	return $this->getParameter('returnUrl');
+    }
+    
+    public function setCallBackUrl($value)
+    {
+    	return $this->setParameter('returnUrl', $value);
+    }
+    
+    public function getOrderId()
+    {
+        return $this->getParameter('OrderId');
+    }
+
+    public function setOrderId($value)
+    {
+        return $this->setParameter('OrderId', $value);
+    }
+
+    public function getCustomerId()
+    {
+        return $this->getParameter('CustomerId');
+    }
+
+    public function setCustomerId($value)
+    {
+        return $this->setParameter('CustomerId', $value);
+    }
+
+    public function getIndustryType()
+    {
+        return $this->getParameter('IndustryType');
+    }
+
+    public function setIndustryType($value)
+    {
+        return $this->setParameter('IndustryType', $value);
+    }
+
+    public function getChannelId()
+    {
+        return $this->getParameter('ChannelId');
+    }
+
+    public function setChannelId($value)
+    {
+        return $this->setParameter('ChannelId', $value);
+    }
+
+    public function getTransactionAmount()
+    {
+        return $this->getParameter('TransactionAmount');
+    }
+
+    public function setTransactionAmount($value)
+    {
+        return $this->setParameter('TransactionAmount', $value);
+    }
+
+    public function getWebsite()
+    {
+        return $this->getParameter('Website');
+    }
+
+    public function setWebsite($value)
+    {
+        return $this->setParameter('Website', $value);
+    }
+
+    public function getMerchantKey()
+    {
+        return $this->getParameter('MerchantKey');
+    }
+
+    public function setMerchantKey($value)
+    {
+        return $this->setParameter('MerchantKey', $value);
+    }
+
+    public function purchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Paytm\Message\PurchaseRequest', $parameters);
+    }
+
+    public function completePurchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Paytm\Message\CompletePurchaseRequest', $parameters);
+    }
+}
---
+++ omnipay-paytm/src/Message/PurchaseRequest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Omnipay\Paytm\Message;
+
+use Omnipay\Common\Message\AbstractRequest;
+use Omnipay\Paytm\PaytmHelpers;
+
+/**
+ * Paytm Purchase Request
+ */
+class PurchaseRequest extends AbstractRequest
+{
+    use PaytmHelpers;
+
+    protected $liveEndpoint = 'https://secure.paytm.in/oltp-web/processTransaction';
+    protected $testEndpoint = 'https://pguat.paytm.com/oltp-web/processTransaction';
+
+    public function getData()
+    {
+        $data['MID'] = $this->getMID();
+        $data['ORDER_ID'] = $this->getOrderId();
+        $data['CUST_ID'] = $this->getCustomerId();
+        $data['INDUSTRY_TYPE_ID'] = $this->getIndustryType();
+        $data['CHANNEL_ID'] = $this->getChannelId();
+        $data['TXN_AMOUNT'] = $this->getTransactionAmount();
+        $data['WEBSITE'] = $this->getWebsite();
+        $data['CALLBACK_URL'] = $this->getCallBackUrl();
+        $data['MOBILE_NO'] = $this->getMobileNum();
+        $data['EMAIL'] = $this->getEmail();
+        $checkSumHash = $this->getChecksumHash($data);
+        $data['CHECKSUMHASH'] = $checkSumHash;
+        return $data;
+    }
+
+    public function sendData($data)
+    {
+        return $this->response = new PurchaseResponse($this, $data);
+    }
+
+    public function getEndpoint()
+    {
+        return $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
+    }
+	
+    public function getMobileNum()
+    {
+    	return $this->getParameter('phoneNum');
+    }
+    
+    public function setMobileNum($value)
+    {
+    	return $this->setParameter('phoneNum', $value);
+    }
+    
+    public function getEmail()
+    {
+    	return $this->getParameter('email');
+    }
+    
+    public function setEmail($value)
+    {
+    	return $this->setParameter('email', $value);
+    }
+    public function getMID()
+    {
+        return $this->getParameter('MID');
+    }
+
+    public function setMID($value)
+    {
+        return $this->setParameter('MID', $value);
+    }
+
+    public function getOrderId()
+    {
+        return $this->getParameter('OrderId');
+    }
+
+    public function setOrderId($value)
+    {
+        return $this->setParameter('OrderId', $value);
+    }
+
+    public function getCallBackUrl()
+    {
+    	return $this->getParameter('returnUrl');
+    }
+    
+    public function setCallBackUrl($value)
+    {
+    	return $this->setParameter('returnUrl', $value);
+    }
+    
+    public function getCustomerId()
+    {
+        return $this->getParameter('CustomerId');
+    }
+
+    public function setCustomerId($value)
+    {
+        return $this->setParameter('CustomerId', $value);
+    }
+
+    public function getIndustryType()
+    {
+        return $this->getParameter('IndustryType');
+    }
+
+    public function setIndustryType($value)
+    {
+        return $this->setParameter('IndustryType', $value);
+    }
+    public function getChannelId()
+    {
+        return $this->getParameter('ChannelId');
+    }
+
+    public function setChannelId($value)
+    {
+        return $this->setParameter('ChannelId', $value);
+    }
+
+    public function getTransactionAmount()
+    {
+        return $this->getParameter('TransactionAmount');
+    }
+
+    public function setTransactionAmount($value)
+    {
+        return $this->setParameter('TransactionAmount', $value);
+    }
+
+    public function getWebsite()
+    {
+        return $this->getParameter('Website');
+    }
+
+    public function setWebsite($value)
+    {
+        return $this->setParameter('Website', $value);
+    }
+
+    public function getMerchantKey()
+    {
+        return $this->getParameter('MerchantKey');
+    }
+
+    public function setMerchantKey($value)
+    {
+        return $this->setParameter('MerchantKey', $value);
+    }
+
+    public function getChecksumHash($data)
+    {
+        $merchantKey = $this->getParameter('MerchantKey');
+        return $this->getChecksumFromArray($data, $merchantKey);
+    }
+}
